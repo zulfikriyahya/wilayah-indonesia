b75685e993b74fc45475052f35f425fc72bf6788
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2025-10-29 08:29:19 +0700

    Init

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: "standalone",
+  experimental: {
+    // Optimize for production
+  },
 };
 
 export default nextConfig;
